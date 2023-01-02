2d46713d0e29d0109e045c9d6cbd9cbef5b92b8d
lizhiwei <lizhiwei@movee.cn>
2023-01-02 20:26:09 +0800

    feat: bugx修复

--- app/controller/mini.js
+++ app/controller/mini.js
@@ -11,13 +11,29 @@ class MiniController extends Controller {
   async whimsyResult() {
     const { ctx, app } = this;
     const { id } = ctx.request.body
+    // 生成随机wxid
+    let wxid = 'wxac8b5749504' + (Math.random() * 100000)
+    wxid = wxid.slice(0, 18)
+    console.log('wxid: ', wxid);
     try {
       const url = `https://z.wxysbd.com/api/Result/index/id/${id}`
       const result = await axios({
         url: url,
         method: 'POST',
         data: ctx.request.body,
-        headers: { 'Referer': 'https://servicewechat.com/wxac12saaddddaf361/devtools/page-frame.html' },
+        headers: {
+          'Referer': `https://servicewechat.com/${wxid}/devtools/page-frame.html`,
+          'Accept': '*/*',
+          'Accept-Encoding': 'gzip, deflate, br',
+          Connection: 'keep-alive',
+          'Content-Length': 45,
+          'content-type': 'application/x-www-form-urlencoded',
+          Host: 'z.wxysbd.com',
+          'Sec-Fetch-Dest': 'empty',
+          'Sec-Fetch-Mode': 'cors',
+          'Sec-Fetch-Site': 'cross-site',
+          'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/604.1 wechatdevtools/1.06.2209190 MicroMessenger/8.0.5 webview/'
+        }
       })
       const data = result?.data?.data
       const qrResult = await qrDecode(data)
